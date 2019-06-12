393227b796a943f1643c1b3562e0a657bd872206
alirezadavooditeacher <51501683+alirezadavooditeacher@users.noreply.github.com>
2019-06-12 07:54:22 -0700

    Activity-Week1
    In-class coding practice for the Java course - week1

---
+++ Activity-Week1/ApplicationDriver.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ApplicationDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Testing problem 1
+//		String word = Problem1.readFromInput();
+//		int reversedNumber = Problem1.reverseIt(word);
+//		System.out.println("The reverse is:"+reversedNumber);
+		
+		//Test problem 2
+		
+//		System.out.println("Enter the first number");
+//		Scanner s = new Scanner(System.in);
+//		String num1 = s.nextLine();
+//		
+//		System.out.println("Enter the second number");
+//		String num2 = s.nextLine();
+//		
+//		int A = Integer.parseInt(num1);
+//		int B = Integer.parseInt(num2);
+//		
+//		Problem2.printNumbersDivisibleBy3And5(A, B);
+//		Problem2.printNumbersDivisibleEitherTo6Or7(A, B);
+//		Problem2.printNumbersNotDivisibleBy3(A, B);
+		
+		//Test problem 3
+		
+//		System.out.println("The binary is: "+Problem3.convertToBinary(0));
+//		System.out.println("The binary is: "+Problem3.convertToBinary(7));
+//		System.out.println("The binary is: "+Problem3.convertToBinary(1));
+//		System.out.println("The binary is: "+Problem3.convertToBinary(64));
+//		System.out.println("The binary is: "+Problem3.convertToBinary(127));
+//		System.out.println("The binary is: "+Problem3.convertToBinary(2));
+//		System.out.println("The binary is: "+Problem3.convertToBinary(234));
+		
+		//Test Problem 4
+		Problem4.run();
+	}
+
+}
