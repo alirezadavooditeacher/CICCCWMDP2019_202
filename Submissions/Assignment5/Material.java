9a03214cc9504c9bd38fb077a44e9f50aee7672b
Ligang <tamader0817@gmail.com>
2019-07-04 16:56:55 -0700

    add assignment5

---
+++ Submissions/Assignment5/Material.java
@@ -0,0 +1,10 @@
+public class Material {
+
+    Integer MaterialCode;
+    String MaterialName;
+
+    public Material(Integer materialCode, String materialName) {
+        MaterialCode = materialCode;
+        MaterialName = materialName;
+    }
+}
