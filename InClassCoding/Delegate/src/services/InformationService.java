bea5582fdeb7f333c9889a2df842f00ab7fbab9d
Vlad <vladi00345@gmail.com>
2019-07-05 20:19:46 -0700

    Duplicated Delegate for problem 2

---
+++ InClassCoding/Delegate/src/services/InformationService.java
@@ -0,0 +1,14 @@
+package services;
+
+public class InformationService {
+    private String notification;
+
+    public String getNotification() {
+        return notification;
+    }
+
+    public InformationService(String notification) {
+        super();
+        this.notification = notification;
+    }
+}
