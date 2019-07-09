513e41065a74d03deee9d5d6fbc9232962d700b3
gsuamar <guillermosuarezmartinez1@gmail.com>
2019-07-08 22:30:45 -0700

    working in program2;table print info method in process

--- Submissions/Assignment6/Program2/src/viewAndTable/Table.java
+++ Submissions/Assignment6/Program2/src/viewAndTable/Table.java
@@ -1,6 +1,7 @@
 package viewAndTable;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Table {
 
@@ -14,6 +15,12 @@ public class Table {
 	public void printData() {
 		ArrayList<String[]> t = this.recieveData();
 		int tableRows=t.size();
-		
+		Iterator<String[]> it = t.iterator();
+		while(it.hasNext()) {
+			String[] indivInfo = it.next();
+			System.out.println("----------------------------------");
+			System.out.println(indivInfo[0]+" "+indivInfo[1]+", "+indivInfo[2]);
+			System.out.println("----------------------------------");
+		}
 	}
 }
