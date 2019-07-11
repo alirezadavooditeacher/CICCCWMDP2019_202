a82129298e3e5cb260f50aebecdf75d55650e65e
PAULO VICTOR DE CASTRO VIGA <paulo.22.5@gmail.com>
2019-07-11 10:30:00 -0700

    Sending you the Assigment 5

---
+++ Submissions/Assigment_5/src/app/Cloth.java
@@ -0,0 +1,62 @@
+package app;
+
+import java.util.ArrayList;
+
+public class Cloth extends Product {
+  private Integer clothId;
+  private String clothName;
+  private Float clothPrice;
+  private String clothMadeIn;
+  private ArrayList<Material> clothMaterials;
+
+  public Cloth(Integer clothId, String clothName, Float clothPrice, String clothMadeIn,
+      ArrayList<Material> clothMaterials) {
+    super(clothId, clothName, clothPrice, clothMadeIn);
+    this.clothId = clothId;
+    this.clothName = clothName;
+    this.clothPrice = clothPrice;
+    this.clothMadeIn = clothMadeIn;
+    this.clothMaterials = clothMaterials;
+  }
+
+  public Integer getClothId() {
+    return this.clothId;
+  }
+
+  public void setClothId(Integer clothId) {
+    this.clothId = clothId;
+  }
+
+  public String getClothName() {
+    return this.clothName;
+  }
+
+  public void setClothName(String clothName) {
+    this.clothName = clothName;
+  }
+
+  public Float getClothPrice() {
+    return this.clothPrice;
+  }
+
+  public void setClothPrice(Float clothPrice) {
+    this.clothPrice = clothPrice;
+  }
+
+  public String getClothMadeIn() {
+    return this.clothMadeIn;
+  }
+
+  public void setClothMadeIn(String clothMadeIn) {
+    this.clothMadeIn = clothMadeIn;
+  }
+
+  public ArrayList<Material> getClothMaterials() {
+    return this.clothMaterials;
+  }
+
+  public void setClothMaterials(ArrayList<Material> clothMaterials) {
+    this.clothMaterials = clothMaterials;
+  }
+
+}
