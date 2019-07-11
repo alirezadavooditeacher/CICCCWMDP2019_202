4fec8ccae0bd08a21d4227192be47be1ef107f7b
PAULO VICTOR DE CASTRO VIGA <paulo.22.5@gmail.com>
2019-07-11 10:39:54 -0700

    Add problem 0

---
+++ Submissions/JavaAssigment_6/Problem0/CarReparingService.java
@@ -0,0 +1,18 @@
+package Services;
+
+public class CarReparingService {
+	
+	private String description;
+	
+	private String carModel;
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public String getCarModel() {
+		return carModel;
+	}
+
+
+}
