aadaab6544efc7ccb2b124f05cacb05c608efef5
Vlad <vladi00345@gmail.com>
2019-07-11 21:23:23 -0700

    Finished problem 1

--- Submissions/Assignment 7/src/Main.java
+++ Submissions/Assignment 7/src/Main.java
@@ -1,6 +1,24 @@
+import Problem1.BookMapper;
+import Problem1.Counter;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        BookMapper book1 = new BookMapper("George Orwell", "1984", 1949);
+        BookMapper book2 = new BookMapper("Victor Hugo", "Les Miserables", 1862);
+        BookMapper book3 = new BookMapper("George R.R.Martin", "A Feast For Crows", 2005);
+        BookMapper book4 = new BookMapper("George Orwell", "Animal Farm", 1945);
+
+        ArrayList<BookMapper> bookList = new ArrayList<>();
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book3);
+        bookList.add(book4);
+
+        Counter<BookMapper> counter = new Counter<>(bookList);
+        System.out.println(counter.countNumberOfElementsWithSpecificProperty("Author", "George Orwell"));
     }
 }
