f5460b2ba35dd2457d181b94f410b231294a702c
PAULO VICTOR DE CASTRO VIGA <paulo.22.5@gmail.com>
2019-07-30 12:21:24 -0700

    Assigment 9 part1

---
+++ Submissions/Assigment 9/src/Problem01/ApplicationDriver.java
@@ -0,0 +1,15 @@
+package Problem01;
+
+public class ApplicationDriver {
+
+
+	    public static void main(String[] args) {
+	        Foo1.Test64 test64 = new Foo1.Test64();
+	        test64.main();
+	    }
+	    
+	    /* bar1 = 5
+			((Foo1)this).bar1 = 60
+			Foo1.this.bar1 = 5
+		*/
+}
---
+++ Submissions/Assigment 9/src/Problem01/Foo1.java
@@ -0,0 +1,32 @@
+package Problem01;
+
+public class Foo1 {
+	
+    private int bar1;
+
+    public Foo1(int bar1) {
+        this.bar1 = bar1;
+    }
+    
+    public class Foo2 extends Foo1{
+        public Foo2(int x) {
+            super(x * 10);
+        }
+
+        public void show() {
+            System.out.println("bar1 = " + bar1);
+            System.out.println("((Foo1)this).bar1 = " + ((Foo1)this).bar1);
+            System.out.println("Foo1.this.bar1 = " + Foo1.this.bar1);
+        }
+    }
+    
+    public static class Test64 {
+        public void main() {
+
+            Foo1 f1 = new Foo1(5);
+            Foo1.Foo2 f2 = f1.new Foo2(6);
+            f2.show();
+        }
+    }
+
+}
