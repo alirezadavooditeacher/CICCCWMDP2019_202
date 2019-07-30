b013e830838b5db3dba45c3b09285f4cc653b29b
gsuamar <guillermosuarezmartinez1@gmail.com>
2019-07-30 00:43:47 -0700

    java hw8, working on program1, fix minor error in class SearchBook; the following is to do appDriver case test

--- Submissions/Assignment8/src/program1/SearchBook.java
+++ Submissions/Assignment8/src/program1/SearchBook.java
@@ -6,15 +6,15 @@ import java.util.Iterator;
 public class SearchBook {
 	
 	public int search(Book book) {
-		ArrayList<Page> bPages = 
+//		ArrayList<Page> bPages = 
 				book.getPageArray();
 		Iterator<Page> it = 
 				 book.iterator();
 		int evenImagePages = 0;
-		while (it.hastNext()) {
+		while (it.hasNext()) {
 			Page pag = it.next();
-			if (pag.getPageNumber%2==0
-					&& pag.getHasImage)
+			if (pag.getPageNumber()%2==0
+					&& pag.isHasImage())
 				evenImagePages++;
 		}
 		return evenImagePages;
