e1f98a162012fa27e92de5faaa6a579377e82851
PAULO VICTOR DE CASTRO VIGA <paulo.22.5@gmail.com>
2019-08-06 12:41:39 -0700

    Create Transformer.java
    Add transformer class

---
+++ Submissions/Assigment11/src/Problem2/Transformer.java
@@ -0,0 +1,97 @@
+package Problem2;
+
+public class Transformer {
+
+	private String name;
+	private int strength;
+	private int intelligence;
+	private int speed;
+	private int endurance;
+	private int firepower;
+	private Team team;
+	
+	public Transformer(String name, int strength, int itelligence, int speed, int endurance, int firepower, Team team) {
+		super();
+		this.name = name;
+		this.strength = strength;
+		this.intelligence = itelligence;
+		this.speed = speed;
+		this.endurance = endurance;
+		this.firepower = firepower;
+		this.team = team;
+	}
+	
+	@Override
+	public String toString() {
+		return "Transformer: [name=" + name + ", team=" + team + ", rank= " +  getRank()+"]";
+	}
+
+	public enum Team{
+		Autobot,
+		Decption
+	}
+	
+    public int getRank() {
+        return strength + intelligence + speed + endurance + firepower;
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+
+	public void setStrenght(int stenght) {
+		this.strength = stenght;
+	}
+
+	public int getIntelligence() {
+		return intelligence;
+	}
+
+	public void setIntelligence(int itelligence) {
+		this.intelligence = itelligence;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getEndurance() {
+		return endurance;
+	}
+
+	public void setEndurance(int endurance) {
+		this.endurance = endurance;
+	}
+
+	public int getFirepower() {
+		return firepower;
+	}
+
+	public void setFirepower(int firepower) {
+		this.firepower = firepower;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+	
+	
+	
+}
