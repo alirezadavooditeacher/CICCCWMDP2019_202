f396052b9ce4aca39fa7578868e763ccf90099bb
gsuamar <guillermosuarezmartinez1@gmail.com>
2019-08-06 16:41:24 -0700

    java hw11 transformer, in war Class batlle method is finish

--- Submissions/Assignment11/src/transformersWars/War.java
+++ Submissions/Assignment11/src/transformersWars/War.java
@@ -22,9 +22,42 @@ public class War {
 	}
 	BiFunction<Transformer, Transformer, String> brule1 = (ta, td) ->{
 		String r1Winner = null;
-		
+		int cDiff = ta.getCourage() - td.getCourage(); 
+		int sDiff = ta.getStrength() - td.getStrength();
+		if(cDiff >= 4 && sDiff >= 3 ) {
+			r1Winner="A";	
+		}
+		else if (cDiff <= -4 && sDiff <=-3) {
+			r1Winner = "D";
+		}
 		return r1Winner;
 	};
+	BiFunction<Transformer, Transformer, String> brule2 = (ta, td) ->{
+		String r2Winner = null;
+		int skDiff = ta.getSkill() - td.getSkill();
+		if(skDiff >=3) {
+			r2Winner="A";
+		}
+		else if(skDiff <=-3) {
+			r2Winner="D";
+		}
+		return r2Winner;
+	};
+	
+	
+	
+	BiConsumer<SolutionDisplay,Transformer> bForA = (sol, td) -> {
+		sol.battleCountUp();
+		sol.recordBattleResult("A");
+		td.setDestroyed(true);
+	};
+	
+	BiConsumer<SolutionDisplay, Transformer> bForD =(sol, ta) ->{
+		sol.battleCountUp();
+		sol.recordBattleResult("D");
+		ta.setDestroyed(true);
+	};
+	
 	MyBiConsumer<SolutionDisplay,Transformer> battle = (sol,ta,td) -> {
 		if (ta.getName() == "Optimus Prime" && td.getName() == "Predaking") {
 			for (Transformer tr : autobot) {
@@ -36,17 +69,38 @@ public class War {
 			this.isTheEnd = true;
 		}
 		else if(ta.getName() == "Optimus Prime") {
-			sol.battleCountUp();
-			sol.recordBattleResult("A");
-			td.setDestroyed(true);
+			bForA.accept(sol, td);
 		}
 		else if(td.getName() == "Predaking") {
-			sol.battleCountUp();
-			sol.recordBattleResult("D");
-			ta.setDestroyed(true);
+			bForD.accept(sol, ta);
 		}
 		else if(null != brule1.apply(ta , td) ) {
-			
+			String r1Winner = brule1.apply(ta, td);
+			if(r1Winner == "A") {
+				bForA.accept(sol, td);
+			}
+			else if (r1Winner == "D") {
+				bForD.accept(sol, ta);
+			}	
+		}
+		else if(null != brule2.apply(ta , td) ) {
+			String r2Winner = brule1.apply(ta, td);
+			if(r2Winner == "A") {
+				bForA.accept(sol, td);
+			}
+			else if (r2Winner == "D") {
+				bForD.accept(sol, ta);
+			}	
+		}
+		else if(ta.getRank() == td.getRank()) {
+			sol.battleCountUp();
+			ta.setDestroyed(true);
+			td.setDestroyed(true);
+		}
+		else if(ta.getRank() > td.getRank()) {
+			bForA.accept(sol, td);
+		}else {
+			bForD.accept(sol, ta);
 		}
 		
 	};
