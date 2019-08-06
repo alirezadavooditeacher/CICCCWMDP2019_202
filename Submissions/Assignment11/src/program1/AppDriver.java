cb13a91f5a10f820e554f9944010089277a76d0d
gsuamar <guillermosuarezmartinez1@gmail.com>
2019-08-05 20:42:46 -0700

    java hw11 finish program 1

--- Submissions/Assignment11/src/program1/AppDriver.java
+++ Submissions/Assignment11/src/program1/AppDriver.java
@@ -8,7 +8,7 @@ public class AppDriver {
 	
 	public static void main(String[] args) {
 		
-		int[] land1 = {1,1,1,2,1,1,1}; //land with one peak, count would be 1 + initial spot = 2:
+		int[] land1 = {1,1,1,2,1,2,1}; //land with one peak, count would be 1 + initial spot = 2:
 		
 		Function<int[],Integer> p1Solution = (e) -> {
 			Integer answer = 0;
@@ -17,12 +17,15 @@ public class AppDriver {
 				answer++;
 			}
 			//Predicates to build if statements
-			BiPredicate<Integer, Integer> bpj = (e1,e2) -> e1 > e2 ; // read: bp1 MEANS bigger than plus j
-			
-			
-			BiPredicate<Integer, Integer> bm1 = (e1,e0) -> e1 > e0 ; // read: bm1 MEANS bigger than minus 1
+//			BiPredicate<Integer, Integer> bpj = (e1,e2) -> e1 > e2 ; // read: bp1 MEANS bigger than plus j
+//			
+//			
+//			BiPredicate<Integer, Integer> bm1 = (e1,e0) -> e1 > e0 ; // read: bm1 MEANS bigger than minus 1
 			
 			BiPredicate<Integer, Integer> sm1 = (e1,e0) -> e1 == e0 ; //read: sm1 MEANS same as minus 1
+			
+			BiPredicate<Integer, Integer> spj = (e2,e1) -> e2 == e1 ; //read: sm1 MEANS same as plus j
+			
 			
 			//My own version of Predicate that has an array as input.
 			
@@ -32,20 +35,35 @@ public class AppDriver {
 			
 // trigger of process: e0 != e1
 //						//then if e2 ==e1 , check if e3==e1 and so on...when e(i+j) != e1 , check if (bpj AND bm1)			
-
-			for(int i = 1; i<=(lSize-1); i++) {
-				//particularcase of 1 integer peak/valley
-				if(
-						
-						
-						!(sm1.test(e[i], e[i-1]))
-					) {
+			int i = 1;
+			int j = 2;
+			while (i<lSize-1 && j<=lSize-1) {
+				if(sm1.test(e[i], e[i-1])){
+					i++;
+					j++;
+				}
+				else if(mvp.test(e[i-1], e[i], e[j])) {
+					answer++;
+					i=j;
+					j++;
+				}
+				else if (spj.test(e[j], e[i])) {
+					j++;
+				}
+				else if(mvp.test(e[i-1],e[i], e[j])) {
+					answer++;
+					i=j;
+					j++;
+				}
+				else {
+					i=j;
+					j=i+1;
 					
-					answer++;}
-	
+				}
 			}
 			return answer;
 		};
+		System.out.println("Expected: 4  Actual: "+p1Solution.apply(land1));
 	}
 
 }
